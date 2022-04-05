03e959befa9ad536a593e0ce212b068f51df1d42
Sithai <trupachello@gmail.com>
2022-04-05 22:31:09 +0300

    Обновил настройки Hibernate

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -41,6 +41,7 @@ public class Util {
                 settings.put(Environment.PASS, dbPassword);
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
                 settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.FORMAT_SQL, "true");
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
                 settings.put(Environment.HBM2DDL_AUTO, "creat-drop");
 
